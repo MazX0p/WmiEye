e322e04ab2e2217bfbe06d8dc19625433c6cea9b
xMohamed <54814433+MazX0p@users.noreply.github.com>
2023-04-06 02:31:52 +0300

    Create config.h

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define LOG_NAME "System"
+#define ELASTICSEARCH_URL "http://localhost:9200/events/_doc"
+#define BOOKMARK_FILE "bookmarks.dat"
+
+#endif /* CONFIG_H */
